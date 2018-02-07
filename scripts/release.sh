07aadfdfad4b8bcf4b9d17420b12f80f80f543f1
Tom Wayson <tom@tomwayson.com>
2018-02-06 21:44:38 -0800

    add comment to release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -7,6 +7,7 @@ carriage-return --level "$CHANGE"
 
 # bump version 
 # NOTE: force is needed so that the preversion script can run
+# and preversion creates production build and runs tests
 npm version "$CHANGE" --force
 
 # push version commit and tags to github
