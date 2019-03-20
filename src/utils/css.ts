8b8a20c588ace424baa3071f5fd18a13d9dc217b
Tom Wayson <tom@tomwayson.com>
2019-03-20 16:44:57 -0700

    add option to insert CSS link before an existing link tag

--- src/utils/css.ts
+++ src/utils/css.ts
@@ -6,19 +6,37 @@ function createStylesheetLink(url) {
   return link;
 }
 
-// TODO: export this function?
+function insertLink(link, before?) {
+  // if we need to insert before an existing link, get all link tags
+  const allLinks = (before || before === 0) && document.getElementsByTagName('link');
+  if (allLinks) {
+    // insert the link before the link tag
+    const beforeLink = allLinks[before];
+    beforeLink.parentNode.insertBefore(link, beforeLink);
+  } else {
+    // append the link to then end of the head tag
+    document.head.appendChild(link);
+  }
+}
+
 // check if the css url has been injected or added manually
 function getCss(url) {
   return document.querySelector(`link[href*="${url}"]`) as HTMLLinkElement;
 }
 
+export interface ILoadCssOptions {
+  url: string;
+  before?: number;
+}
+
 // lazy load the CSS needed for the ArcGIS API
-export function loadCss(url) {
+export function loadCss(css: string | ILoadCssOptions) {
+  const url = typeof css === 'string' ? css : css.url;
   let link = getCss(url);
   if (!link) {
-    // create & load the css library
+    // create & load the css link
     link = createStylesheetLink(url);
-    document.head.appendChild(link);
+    insertLink(link, (css as ILoadCssOptions).before);
   }
   return link;
 }
