6a9466ea1a6de076b6506931845112ec370332c3
Tom Wayson <tom@tomwayson.com>
2019-03-25 22:14:19 -0700

    added util fns to get CDN URLs and tests

---
+++ src/utils/url.test.ts
@@ -0,0 +1,33 @@
+import { getCdnCssUrl, getCdnUrl } from './url';
+
+describe ('when getting CDN URLs', () => {
+  describe('for the script', () => {
+    describe('with no arguments', () => {
+      it('should default to latest 4.x URL', () => {
+        expect(getCdnUrl()).toEqual('https://js.arcgis.com/4.10/');
+      });
+    });
+    describe('with a valid version', () => {
+      it('should return URL for that version', () => {
+        expect(getCdnUrl('3.27')).toEqual('https://js.arcgis.com/3.27/');
+      });
+    });
+  });
+  describe('for the CSS', () => {
+    describe('with no arguments', () => {
+      it('should default to the latest 4.x CSS URL', () => {
+        expect(getCdnCssUrl()).toEqual('https://js.arcgis.com/4.10/esri/css/main.css');
+      });
+    });
+    describe('for 3.x version >= 3.11', () => {
+      it('should return the CSS URL for that version', () => {
+        expect(getCdnCssUrl('3.27')).toEqual('https://js.arcgis.com/3.27/esri/css/esri.css');
+      });
+    });
+    describe('for version < 3.11', () => {
+      it('should return the CSS URL for that version', () => {
+        expect(getCdnCssUrl('3.10')).toEqual('https://js.arcgis.com/3.10/js/esri/css/esri.css');
+      });
+    });
+  });
+});
---
+++ src/utils/url.ts
@@ -0,0 +1,36 @@
+const DEFAULT_VERSION = '4.10';
+
+export function parseVersion(version) {
+  const match = version && version.match(/^(\d)\.(\d+)/);
+  return match && {
+    major: parseInt(match[1], 10),
+    minor: parseInt(match[2], 10)
+  };
+}
+
+/**
+ * Get the CDN url for a given version
+ *
+ * @param version Ex: '4.10' or '3.27'. Defaults to the latest 4.x version.
+ */
+export function getCdnUrl(version = DEFAULT_VERSION) {
+  return `https://js.arcgis.com/${version}/`;
+}
+
+/**
+ * Get the CDN url for a the CSS for a given version and/or theme
+ *
+ * @param version Ex: '4.10' or '3.27'. Defaults to the latest 4.x version.
+ */
+export function getCdnCssUrl(version = DEFAULT_VERSION) {
+  const baseUrl = getCdnUrl(version);
+  const parsedVersion = parseVersion(version);
+  if (parsedVersion.major === 3) {
+    // NOTE: at 3.11 the CSS moved from the /js folder to the root
+    const path = parsedVersion.minor <= 10 ? 'js/' : '';
+    return `${baseUrl}${path}esri/css/esri.css`;
+  } else {
+    // assume 4.x
+    return `${baseUrl}esri/css/main.css`;
+  }
+}
