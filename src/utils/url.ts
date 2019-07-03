206610b7fc65430ad66ef2c1bdafa67834a628ba
ziveo <bziveo@gmail.com>
2019-07-03 09:41:55 -0400

    Updated all code & Readme to ArcGIS JavaScript API v4.12 & v3.29

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -1,7 +1,7 @@
 /* Copyright (c) 2017 Environmental Systems Research Institute, Inc.
  * Apache-2.0 */
 
-const DEFAULT_VERSION = '4.11';
+const DEFAULT_VERSION = '4.12';
 
 export function parseVersion(version) {
   const match = version && version.match(/^(\d)\.(\d+)/);
@@ -14,7 +14,7 @@ export function parseVersion(version) {
 /**
  * Get the CDN url for a given version
  *
- * @param version Ex: '4.11' or '3.28'. Defaults to the latest 4.x version.
+ * @param version Ex: '4.12' or '3.29'. Defaults to the latest 4.x version.
  */
 export function getCdnUrl(version = DEFAULT_VERSION) {
   return `https://js.arcgis.com/${version}/`;
@@ -23,7 +23,7 @@ export function getCdnUrl(version = DEFAULT_VERSION) {
 /**
  * Get the CDN url for a the CSS for a given version and/or theme
  *
- * @param version Ex: '4.11' or '3.28'. Defaults to the latest 4.x version.
+ * @param version Ex: '4.12' or '3.29'. Defaults to the latest 4.x version.
  */
 export function getCdnCssUrl(version = DEFAULT_VERSION) {
   const baseUrl = getCdnUrl(version);
