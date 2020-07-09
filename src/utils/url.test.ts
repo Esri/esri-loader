9003315525f1c0976649e04cda8117e3c9530b84
Josh Crozier <crozier4@gmail.com>
2020-07-09 16:03:00 -0400

    Update to ArcGIS JavaScript API v4.16/3.33

--- src/utils/url.test.ts
+++ src/utils/url.test.ts
@@ -4,12 +4,12 @@ describe ('when getting CDN URLs', () => {
   describe('for the script', () => {
     describe('with no arguments', () => {
       it('should default to latest 4.x URL', () => {
-        expect(getCdnUrl()).toEqual('https://js.arcgis.com/4.15/');
+        expect(getCdnUrl()).toEqual('https://js.arcgis.com/4.16/');
       });
     });
     describe('with a valid version', () => {
       it('should return URL for that version', () => {
-        expect(getCdnUrl('3.32')).toEqual('https://js.arcgis.com/3.32/');
+        expect(getCdnUrl('3.33')).toEqual('https://js.arcgis.com/3.33/');
       });
     });
     // TODO: what about an invalid version? should we throw?
@@ -17,12 +17,12 @@ describe ('when getting CDN URLs', () => {
   describe('for the CSS', () => {
     describe('with no arguments', () => {
       it('should default to the latest 4.x CSS URL', () => {
-        expect(getCdnCssUrl()).toEqual('https://js.arcgis.com/4.15/esri/themes/light/main.css');
+        expect(getCdnCssUrl()).toEqual('https://js.arcgis.com/4.16/esri/themes/light/main.css');
       });
     });
     describe('for 3.x version >= 3.11', () => {
       it('should return the CSS URL for that version', () => {
-        expect(getCdnCssUrl('3.32')).toEqual('https://js.arcgis.com/3.32/esri/css/esri.css');
+        expect(getCdnCssUrl('3.33')).toEqual('https://js.arcgis.com/3.33/esri/css/esri.css');
       });
     });
     describe('for version < 3.11', () => {
